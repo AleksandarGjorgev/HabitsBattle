4634f2b18cc3c557eae282d762037ac9c67df3d4
Aleksandar <gjorgevaleks@gmail.com>
2024-05-12 20:58:17 +0200

    Navbar added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
+import Example from "layout/header";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+// YourComponent.tsx
+import React from 'react';
+import Navbar from './navbar.jsx'; // Adjust the path as per your project structure
+
+const Page = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default Page;
